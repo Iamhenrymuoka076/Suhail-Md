0d22562c9e3d95346bd206a68f98b5581d426658
Iamhenrymuoka076 <167719322+Iamhenrymuoka076@users.noreply.github.com>
2024-06-15 10:11:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ global.email ="saimsamsun789@gmail.com"
 global.location="Lahore,Pakistan."
 
 
-global.mongodb= process.env.MONGODB_URI || ""
+global.mongodb= process.env.MONGODB_URI || "mongosh "mongodb+srv://cluster0.n2hlqlw.mongodb.net/" --apiVersion 1 --username henrymuoka03"
 global.allowJids= process.env.ALLOW_JID || "null" 
 global.blockJids= process.env.BLOCK_JID || "null"
 global.DATABASE_URL = process.env.DATABASE_URL || ""
